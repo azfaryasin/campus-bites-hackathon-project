418f4330e765037d29f633c73f951fef9228f531
Azfar Yasin <azfaryasinpersonal@gmail.com>
2025-05-25 17:12:53 +0530

    Update main.tsx

--- quick-eat-corner-main/src/main.tsx
+++ quick-eat-corner-main/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
