79243fd34ecfaa87879225fcc0a87dc37083e8b7
Azfar Yasin <azfaryasinpersonal@gmail.com>
2025-05-25 16:32:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/quick-eat-corner/",
+  base: '/campus-bites-hackathon-project/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
