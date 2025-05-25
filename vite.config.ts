40c58c17a628b3ec70605d7223cdedf6edef7865
Azfar <azfaryasinpersonal@gmail.com>
2025-05-26 00:34:31 +0530

    fix: update deployment configurations

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,6 +11,8 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
