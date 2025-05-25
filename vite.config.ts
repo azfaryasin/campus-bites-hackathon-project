37db98b978a010afe9fe52e0328a8f9655bc3c75
Azfar <azfaryasinpersonal@gmail.com>
2025-05-26 00:30:19 +0530

    fix: update vite and netlify config for proper JS serving

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/quick-eat-corner/",
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
